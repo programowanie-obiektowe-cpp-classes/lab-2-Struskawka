6f36bd1dc4ed488b399403c415d7483cfcb5946a
Struskawka <indi.15.15@gmail.com>
2022-10-25 07:26:29 +0000

    instrukcja

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,9 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+   /*- jest właścicielem (czyt. zarządza) obiektu typu `Resource`
+- posiada metodę `double get()`, która zwraca wynik zawołania metody `get` obiektu, którym zarządza
+- posiada konstruktor domyślny, który inicjalizuje obiekt typu `Resource` tak, aby nie zarządzał żadnym zasobem
+- posiada 5 dobrze zdefiniowanych metod specjalnych
+- jest mała, tzn. mieści się na stosie*/
 };
