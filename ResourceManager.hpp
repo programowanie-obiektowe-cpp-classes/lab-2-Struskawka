150a342fafb47c287fec121ec33f4b47706bf9b5
Struskawka <indi.15.15@gmail.com>
2022-10-25 07:56:53 +0000

    po labolatorium

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,9 +4,29 @@
 
 class ResourceManager
 {
-   /*- jest właścicielem (czyt. zarządza) obiektu typu `Resource`
-- posiada metodę `double get()`, która zwraca wynik zawołania metody `get` obiektu, którym zarządza
-- posiada konstruktor domyślny, który inicjalizuje obiekt typu `Resource` tak, aby nie zarządzał żadnym zasobem
-- posiada 5 dobrze zdefiniowanych metod specjalnych
-- jest mała, tzn. mieści się na stosie*/
+
+//- jest właścicielem (czyt. zarządza) obiektu typu `Resource`
+//- posiada metodę `double get()`, która zwraca wynik zawołania metody `get` obiektu, którym zarządza
+double get()
+
+//- posiada konstruktor domyślny, który inicjalizuje obiekt typu `Resource` tak, aby nie zarządzał żadnym zasobem
+ResourceManager(double dk) : k{dk} {}
+Resource k;
+
+//- posiada 5 dobrze zdefiniowanych metod specjalnych
+//- jest mała, tzn. mieści się na stosie
+
 };
+
+
+/*
+Masz daną klasę `Resource`, która:
+
+- posiada konstruktor domyślny
+- posiada 5 dobrze zdefiniowanych metod specjalnych
+- posiada metodę o sygnaturze `double get()`
+- jest duża, tzn. wartość `sizeof(Resource)` jest większa niż rozmiar stosu
+
+Klasa ta symuluje duży, kosztowny w konstrukcji zasób.
+Metoda `get` symuluje wykorzystanie takiego zasobu (np. pobranie wartości zmiennoprzecinkowej z serwera).
+*/
